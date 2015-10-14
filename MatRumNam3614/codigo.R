a15461adad0047e6ee2ad224502142171d5c0c46
reyzaguirre <reyzaguirre@gmail.com>
2015-10-14 13:09:58 -0500

    actualizado a fecha 2

--- MatRumNam3614/codigo.R
+++ MatRumNam3614/codigo.R
@@ -14,8 +14,10 @@ resu <- simula(nelim)
 
 # Guardar simulaciones para usar luego
 
-save(resu, file= "MatRumNam3614/simulaciones.RData")
+save(resu, file= "MatRumNam3614/simulaciones.RData") # considera resultados hasta 2014
+save(resu, file= "MatRumNam3614/simulaciones2.RData") # considera resultados hasta la ultima fecha
 load("MatRumNam3614/simulaciones.RData")
+load("MatRumNam3614/simulaciones2.RData")
 
 # probabilidad de clasificar
 
@@ -25,6 +27,8 @@ pclas <- sort(table(c(resu[[2]][,1], resu[[2]][,2], resu[[2]][,3], resu[[2]][,4]
 ddd <- data.frame(pais = c("Argentina", "Brasil", "Paraguay", "Colombia", "Ecuador",
                            "Uruguay", "Chile", "Perú", "Venezuela", "Bolivia"),
                   pclas = pclas)
+
+# Grafico. Guardar en 950 x 600
 
 color <- "darkorange1"
 barplot(ddd$pclas, space = 0.8, names.arg = ddd$pais,
@@ -75,3 +79,21 @@ apply(resu[[1]], 2, mean)
 # 95% de confianza para puntos de peru
 
 quantile(resu[[1]][,8], probs = c(0.025, 0.975), type=7)
+
+# Considerando los datos de Rusia 2018 para Qatar 2022
+
+pclas <- sort(table(c(resu[[2]][,1], resu[[2]][,2], resu[[2]][,3], resu[[2]][,4], resu[[2]][,5])),
+              decreasing = TRUE)/nelim
+
+ddd <- data.frame(pais = c("Argentina", "Brasil", "Paraguay", "Ecuador", "Colombia",
+                           "Uruguay", "Chile", "Perú", "Venezuela", "Bolivia"),
+                  pclas = pclas)
+
+barplot(ddd$pclas, space = 0.8, names.arg = ddd$pais, ylim = c(0,1),
+        col = c(rep(color, 7), 'red', rep(color, 2)))
+title(main = "Probabilidad de clasificar a Qatar 2022", font.main = 4)
+mtext("(El modelo incluye hasta los resultados de la fecha 2 de la clasificatoria Rusia 2018)",
+      side = 3, line = 0.4, cex = 0.9)
+mtext("AR: 0.929, BR: 0.884, PA: 0.660, EC: 0.641, CO: 0.640, UR: 0.542, CH: 0.518, PE: 0.080, VEN: 0.062, BO: 0.043",
+      side = 1, line = 3, cex = 0.9)
+      
